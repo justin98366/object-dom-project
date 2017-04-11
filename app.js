4d7564217b66748a6840a6d8bf816eed470b250c
Justin Green <justin98366@gmail.com>
2017-04-11 13:28:41 -0700

    forgot to add totals--added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 var storeHours = ['6am', '7am', '8am', '9am' , '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-var counter = 0;
+//var counter = 0;
 var pikeStore = document.getElementById('andPike');
 var seaTacStore = document.getElementById('seaTac');
 var seaCentStore = document.getElementById('seattleCenter');
@@ -18,15 +18,22 @@ var pikeLoc = {
   },
 
   storeCalc: function(){
+    var counter = 0;
     for (var i = 0; i < 15; i++){
       var averageCalc = Math.round(this.avCookie * this.rndCust());
       this.results.push(averageCalc);
       var liEl = document.createElement('li');
       liEl.textContent = storeHours[i] + ': ' + pikeLoc.results[i] + ' cookies.';
-
       pikeStore.appendChild(liEl);
+      //var counter = pikeLoc.results[i] + pikeLoc.results[0];
+      counter += pikeLoc.results[i]
+      var totCook = document.createElement('li');
+      totCook.textContent = 'Total cookies: ' + counter;
+
 
     }
+    console.log(counter);
+    pikeStore.appendChild(totCook);
 
   },
 
@@ -42,14 +49,19 @@ var seatacLoc = {
     return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust);
   },
   storeCalc: function(){
+    var counter = 0;
     for (var i = 0; i < 15; i++){
       var averageCalc = Math.round(this.avCookie * this.rndCust());
       this.results.push(averageCalc);
       var liEl = document.createElement('li');
       liEl.textContent = storeHours[i] + ': ' + seatacLoc.results[i] + ' cookies.';
-
       seaTacStore.appendChild(liEl);
+      counter += seatacLoc.results[i]
+      var totCook = document.createElement('li');
+      totCook.textContent = 'Total cookies: ' + counter;
     }
+    console.log(counter);
+    seaTacStore.appendChild(totCook);
 
   },
 };
@@ -63,14 +75,19 @@ var seaCentLoc = {
     return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust);
   },
   storeCalc: function(){
+    var counter = 0;
     for (var i = 0; i < 15; i++){
       var averageCalc = Math.round(this.avCookie * this.rndCust());
       this.results.push(averageCalc);
       var liEl = document.createElement('li');
       liEl.textContent = storeHours[i] + ': ' + seaCentLoc.results[i] + ' cookies.';
-
       seaCentStore.appendChild(liEl);
+      counter += seaCentLoc.results[i]
+      var totCook = document.createElement('li');
+      totCook.textContent = 'Total cookies: ' + counter;
     }
+    console.log(counter);
+    seaCentStore.appendChild(totCook);
 
   },
 };
@@ -84,14 +101,19 @@ var capHillLoc = {
     return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust)
   },
   storeCalc: function(){
+    var counter = 0;
     for (var i = 0; i < 15; i++){
       var averageCalc = Math.round(this.avCookie * this.rndCust());
       this.results.push(averageCalc);
       var liEl = document.createElement('li');
       liEl.textContent = storeHours[i] + ': ' + capHillLoc.results[i] + ' cookies.';
-
       capHillStore.appendChild(liEl);
+      counter += capHillLoc.results[i]
+      var totCook = document.createElement('li');
+      totCook.textContent = 'Total cookies: ' + counter;
     }
+    console.log(counter);
+    capHillStore.appendChild(totCook);
 
   },
 };
@@ -105,14 +127,19 @@ var alkiLoc = {
     return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust)
   },
   storeCalc: function(){
+    var counter = 0;
     for (var i = 0; i < 15; i++){
       var averageCalc = Math.round(this.avCookie * this.rndCust());
       this.results.push(averageCalc);
       var liEl = document.createElement('li');
       liEl.textContent = storeHours[i] + ': ' + alkiLoc.results[i] + ' cookies.';
-
       alkiStore.appendChild(liEl);
+      counter += alkiLoc.results[i]
+      var totCook = document.createElement('li');
+      totCook.textContent = 'Total cookies: ' + counter;
     }
+    console.log(counter);
+    alkiStore.appendChild(totCook);
 
   },
 };
