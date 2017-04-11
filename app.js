11e0d821de1027f12b9d5523c50346f0707c4c6f
Justin Green <justin98366@gmail.com>
2017-04-10 17:42:26 -0700

    added/completed all objects

--- app.js
+++ app.js
@@ -1,5 +1,92 @@
 'use strict';
 
-var pikeLocation = {
-  
-}
+var pikeLoc = {
+  minCust: 23,
+  maxCust: 65,
+  avCookie: 6.3,
+  results: [],
+  rndCust: function() {
+    return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust);
+
+  },
+
+  storeCalc: function(){
+    for (var i = 0; i < 16; i++){
+      var averageCalc = Math.round(this.avCookie * this.rndCust());
+      this.results.push(averageCalc);
+    }
+
+  },
+
+
+};
+
+var seatacLoc = {
+  minCust: 3,
+  maxCust: 24,
+  avCookie: 1.2,
+  results: [],
+  rndCust: function(){
+    return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust);
+  },
+  storeCalc: function(){
+    for (var i = 0; i < 16; i++){
+      var averageCalc = Math.round(this.avCookie * this.rndCust());
+      this.results.push(averageCalc);
+    }
+
+  },
+};
+
+var seaCentLoc = {
+  minCust: 11,
+  maxCust: 38,
+  avCookie: 3.7,
+  results:[],
+  rndCust: function(){
+    return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust);
+  },
+  storeCalc: function(){
+    for (var i = 0; i < 16; i++){
+      var averageCalc = Math.round(this.avCookie * this.rndCust());
+      this.results.push(averageCalc);
+    }
+
+  },
+};
+
+var capHillLoc = {
+  minCust: 20,
+  maxCust: 38,
+  avCookie: 2.3,
+  results: [],
+  rndCust: function(){
+    return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust)
+  },
+  storeCalc: function(){
+    for (var i = 0; i < 16; i++){
+      var averageCalc = Math.round(this.avCookie * this.rndCust());
+      this.results.push(averageCalc);
+    }
+
+  },
+};
+
+var alkiLoc = {
+  minCust: 2,
+  maxCust: 16,
+  avCookie:4.6,
+  results: [],
+  rndCust: function(){
+    return Math.floor((Math.random() * (this.maxCust - this.minCust + 1))+this.minCust)
+  },
+  storeCalc: function(){
+    for (var i = 0; i < 16; i++){
+      var averageCalc = Math.round(this.avCookie * this.rndCust());
+      this.results.push(averageCalc);
+    }
+
+  },
+};
+
+console.log(pikeLoc.storeCalc);
